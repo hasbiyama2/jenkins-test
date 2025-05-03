6d176097b6398a4508200db312f788524fd23d56
hasbiyama2 <hasbi.x64@gmail.com>
2025-05-03 10:35:14 +0700

    Update simple.py

--- simple.py
+++ simple.py
@@ -5,7 +5,7 @@ from datetime import timezone, timedelta
 current_time = datetime.now()
 
 # Print a welcoming message
-print("Hell, User! Welcome!")
+print("Hello, User! Welcome!")
 
 # Print the current date and time
 # Convert the current time to GMT+7
